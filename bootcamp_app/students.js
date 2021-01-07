32d1bd05eb54f6fa7e3f5cd26310f35babc180da
saoussen <sawsan_sli3i@live.fr>
2021-01-07 19:31:26 +0000

    added Parameterized Query to students.js

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -6,13 +6,10 @@ const pool = new Pool({
   host: 'localhost',
   database: 'bootcampx'
 });
-pool.query(`
-SELECT students.id as student_id, students.name as name, cohorts.name as cohort
-FROM students
-JOIN cohorts ON cohorts.id = cohort_id
-WHERE cohorts.name LIKE '%${process.argv[2]}%'
-LIMIT ${process.argv[3] || 5};
-`)
+const cohortName = process.argv[2];
+const limit = process.argv[3] || 5;
+
+pool.query('SELECT students.id as student_id, students.name as name, cohorts.name as cohort FROM students JOIN cohorts ON cohorts.id = cohort_id WHERE cohorts.name = $2 LIMIT $1;', [limit, cohortName])
 .then(res => {
   res.rows.forEach(user => {
     console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);

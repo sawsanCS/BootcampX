e3b50cf038abdcd01969c80cd576fc40e4d9577f
saoussen <sawsan_sli3i@live.fr>
2021-01-07 13:57:44 +0000

    update students.js

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -6,3 +6,13 @@ const pool = new Pool({
   host: 'localhost',
   database: 'bootcampx'
 });
+pool.query(`
+SELECT id, name, cohort_id
+FROM students
+LIMIT 5;
+`)
+.then(res => {
+  res.rows.forEach(user => {
+    console.log(`${user.name} has an id of ${user.id} and was in the ${user.cohort_id} cohort`);
+  })
+});
